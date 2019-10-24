9e5f3ed45aae112663f2e8b2efe66b502aedffbb
Dmitriy Konovalov <dimusig@gmail.com>
2019-10-24 22:54:55 +0300

    Updates

---
+++ precache-manifest.e173eb275c1eb2cdece02ed5a5ea245c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f319ac886116d0cd4181922444b9c00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73b3d3a9f341c70437ae",
+    "url": "/static/css/main.70afe44a.chunk.css"
+  },
+  {
+    "revision": "48a560b5aba69ff8fe0c",
+    "url": "/static/js/2.d2beeb71.chunk.js"
+  },
+  {
+    "revision": "73b3d3a9f341c70437ae",
+    "url": "/static/js/main.d5892f58.chunk.js"
+  },
+  {
+    "revision": "19c4b79ae74e2ad9cc8d",
+    "url": "/static/js/runtime-main.b8389547.js"
+  }
+]);
